d56815c2a4726731906b6ffea8a5aa923e1584ef
Jean Ferreira <Jeansantoz13@gmail.com>
2022-08-21 11:33:17 -0300

    Tratamento de exceptions implementado

--- src/main/java/com/jeandev/cursospring/services/UserService.java
+++ src/main/java/com/jeandev/cursospring/services/UserService.java
@@ -3,11 +3,16 @@ package com.jeandev.cursospring.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.jeandev.cursospring.entities.User;
 import com.jeandev.cursospring.repositories.UserRepository;
+import com.jeandev.cursospring.services.exceptions.DatabaseException;
 import com.jeandev.cursospring.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,13 +35,29 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try{
+            repository.deleteById(id);
+        }
+        catch(EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }
+        catch(DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+        
     }
 
     public User update(Long id, User user){
-        User entity = repository.getOne(id);
-        updateData(entity, user);
-        return repository.save(entity);
+        try{
+            User entity = repository.getOne(id);
+            updateData(entity, user);
+            return repository.save(entity);
+        }
+        catch(EntityNotFoundException e){
+            e.getMessage();
+            throw new ResourceNotFoundException(id);
+        }
+        
     }
 
     private void updateData(User entity, User user) {
